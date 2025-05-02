e1bed774cf54e8c5e269e8a55467df0c6b31b5ec
Lukáš Hajnyš <hajnys.lukas@gmail.com>
2025-05-02 12:56:03 +0200

    Prepsan Program.cs pro podobu vhodnou pro MongoDB

--- PoznamkyApp/PoznamkyApp/Program.cs
+++ PoznamkyApp/PoznamkyApp/Program.cs
@@ -1,39 +1,23 @@
-using Microsoft.EntityFrameworkCore;
 using PoznamkyApp.Data;
 
-namespace PoznamkyApp
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            // Ensure you add DbContext and use the connection string from appsettings.json
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// MongoDB context
+builder.Services.AddSingleton<MongoDbContext>();
 
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddHttpContextAccessor();
-            builder.Services.AddSession();
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession();
 
-            var app = builder.Build();
+var app = builder.Build();
 
-            // Code to test the database in early stages of development
-            //TestDB(app);
-            // Configure the HTTP request pipeline, middleware, etc.
-            app.UseRouting();
-            app.MapDefaultControllerRoute();
-            app.UseSession();
-            app.UseStaticFiles();
-            app.UseAuthorization();
+app.UseRouting();
+app.UseStaticFiles();
+app.UseSession();
+app.UseAuthorization();
 
-            app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Account}/{action=Register}/{id?}");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Account}/{action=Register}/{id?}");
 
-            app.Run();
-        }
-    }
-}
+app.Run();
