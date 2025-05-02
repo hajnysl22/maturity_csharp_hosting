b9f8c862f6bd8c6cb6326899b5b1dbac4eff421a
Lukáš Hajnyš <hajnys.lukas@gmail.com>
2025-05-02 13:05:57 +0200

    Upraven kontext, aby bral připojení z .env souboru

--- PoznamkyApp/PoznamkyApp/Data/AppDbContext.cs
+++ PoznamkyApp/PoznamkyApp/Data/AppDbContext.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using MongoDB.Driver;
+﻿using MongoDB.Driver;
 using PoznamkyApp.Models;
 
 namespace PoznamkyApp.Data
@@ -8,13 +7,18 @@ namespace PoznamkyApp.Data
     {
         private readonly IMongoDatabase _database;
 
-        public MongoDbContext(IConfiguration config)
-        {
-            var client = new MongoClient(config.GetConnectionString("MongoDb"));
-            _database = client.GetDatabase("PoznamkyAppDb");
-        }
-
         public IMongoCollection<User> Users => _database.GetCollection<User>("Users");
         public IMongoCollection<Note> Notes => _database.GetCollection<Note>("Notes");
+
+        public MongoDbContext()
+        {
+            var connectionString = Environment.GetEnvironmentVariable("MONGO_DB");
+
+            if (string.IsNullOrEmpty(connectionString))
+                throw new InvalidOperationException("MongoDB connection string not found in environment variables.");
+
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase("PoznamkyAppDb");
+        }
     }
 }
